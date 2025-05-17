cf590ebcde68118219185323b5faf0d6278a9868
Paulo Cardoso <cardosopaulohenrique91@gmail.com>
2025-05-17 19:44:22 -0300

    feat: models and macros

---
+++ northwind/models/marts/vendas.sql
@@ -0,0 +1,20 @@
+{{ config(
+    materialized='incremental',
+    unique_key='id'
+)}}
+
+with vendas as (
+    select 
+        *
+    from 
+        {{ref('stg_crm__nova_tabela')}}
+)
+
+select * from vendas
+
+{% if is_incremental() %}
+
+    -- Assuming Postgres database
+    where updated_at > (select max(updated_at) from {{this}})
+
+{%endif %}
